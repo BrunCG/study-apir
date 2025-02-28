348e4a2034718c1d5db69d16d782cad6096edae8
0Brun0 <brunodecastrogranado@gmail.com>
2025-02-27 21:00:07 -0300

    feat: add Products

--- src/main/java/com/github/bruncg/study_apir/controller/ControllerProduto.java
+++ src/main/java/com/github/bruncg/study_apir/controller/ControllerProduto.java
@@ -1,4 +1,4 @@
-package com.github.bruncg.study_apir;
+package com.github.bruncg.study_apir.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
