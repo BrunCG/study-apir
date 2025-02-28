4600002c5920713869aef938629efdfae6ba8697
0Brun0 <brunodecastrogranado@gmail.com>
2025-02-27 21:57:47 -0300

    Add some lists

---
+++ src/main/java/com/github/bruncg/study_apir/service/ProductService.java
@@ -0,0 +1,9 @@
+package com.github.bruncg.study_apir.service;
+
+import java.util.List;
+
+import com.github.bruncg.study_apir.model.Product;
+
+public class ProductService {
+    private List<Product> products;
+}
